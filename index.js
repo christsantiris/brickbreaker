1121e3cfda8af1eb46aa6a59610e094dba6b0d73
Chris Tsantiris <christsantiris@Chriss-MacBook-Air.local>
2017-05-07 12:10:50 -0400

    ball bounces off walls if coordinate outside of canvas height and width

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ function init() {
   let y = canvas.height-30;
   let dx = 2;
   let dy = -2;
+  let ballRadius = 10;
 
   function drawBall () {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
+    if (x + dx > canvas.width || x + dx < 0) {
+      dx = -dx;
+    }
+    if (y + dy >canvas.height || y + dy < 0) {
+      dy = -dy;
+    }
   }
 
   function draw () {
